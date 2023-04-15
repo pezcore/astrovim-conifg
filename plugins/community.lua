db336c3a249bc4c9a31b15f5379c465ce6cedf47
pezcore <tvboyd23@gmail.com>
2023-04-15 15:11:38 -0400

    Add some Astrocommunity colorschemes

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,10 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.nightfox"},
+  { import = "astrocommunity.colorscheme.kanagawa"},
+  { import = "astrocommunity.colorscheme.gruvbox"},
+  { import = "astrocommunity.colorscheme.everforest"},
+  { import = "astrocommunity.colorscheme.oxocarbon"},
 }
