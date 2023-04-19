08274f2920188cf3af9575bb45f1825af46b0cb8
pezcore <tvboyd23@gmail.com>
2023-04-18 20:04:09 -0400

    new alpha dashboard

--- plugins/user.lua
+++ plugins/user.lua
@@ -55,4 +55,36 @@ return {
       groups = { all = { NormalFloat = { link = "Normal" } } },
     },
   },
+  {
+    "goolord/alpha-nvim",
+     config = function ()
+       local alpha = require'alpha'
+       local dashboard = require'alpha.themes.dashboard'
+       dashboard.section.header.val = {
+           [[                               __                ]],
+           [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+           [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+           [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+           [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+           [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+       }
+       dashboard.section.buttons.val = {
+           dashboard.button( "e", "  New file"  , ":ene <BAR> startinsert <CR>"),
+           dashboard.button( "f", "  Find File" , ":Tele fd<CR>"),
+           dashboard.button( "o", "  Recents"   , ":Tele oldfiles<CR>"),
+           dashboard.button( "w", "  Find Word" , ":Tele live_grep<CR>"),
+           dashboard.button( "q", "  Quit NVIM" , ":qa<CR>"),
+       }
+       local handle = io.popen('fortune')
+       local fortune = handle:read("*a")
+       handle:close()
+       dashboard.section.footer.val = fortune
+
+       dashboard.config.opts.noautocmd = true
+
+       vim.cmd[[autocmd User AlphaReady echo 'ready']]
+
+       alpha.setup(dashboard.config)
+     end
+ },
 }
